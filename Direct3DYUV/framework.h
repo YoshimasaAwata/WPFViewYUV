1d7cfbb3a6f9ec6c27c7521fb0e91b30b077c104
YoshimasaAwata <yoshia.awata@nifty.ne.jp>
2021-12-25 16:56:21 +0900

    プロジェクト ファイルを追加します。

---
+++ Direct3DYUV/framework.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#define WIN32_LEAN_AND_MEAN             // Windows ヘッダーからほとんど使用されていない部分を除外する
+// Windows ヘッダー ファイル
+#include <windows.h>
+
+#include <d3d9.h>
+#include <d3dx9.h>
+
+#include <assert.h>
+
+#include "RendererManager.h"
+#include "Renderer.h"
+#include "TriangleRenderer.h"
+
+#define IFC(x) { hr = (x); if (FAILED(hr)) goto Cleanup; }
+#define IFCOOM(x) { if ((x) == NULL) { hr = E_OUTOFMEMORY; IFC(hr); } }
+#define SAFE_RELEASE(x) { if (x) { x->Release(); x = NULL; } }
